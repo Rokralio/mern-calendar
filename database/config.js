988fee18cc0cb92fc3f91fff7922151356f3185c
Rokralio <rjgonzalez.1996@gmail.com>
2023-09-05 01:56:43 -0500

    push 1

--- database/config.js
+++ database/config.js
@@ -1,8 +1,9 @@
 const mongoose = require('mongoose');
 
 const dbConnection = async() => {
+
   try {
-    console.log(process.env.DB_CNN);
+
     await mongoose.connect( process.env.DB_CNN,  {
       useNewUrlParser: true, 
       useUnifiedTopology: true,
