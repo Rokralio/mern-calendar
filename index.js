81a717cf8077298935870ef7f98c579bea6dafa5
Rokralio <rjgonzalez.1996@gmail.com>
2023-09-05 16:43:45 -0500

    log prueba

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ app.use( express.json() );
 app.use( '/api/auth', require( './routes/auth' ) );
 app.use( '/api/events', require( './routes/events' ) );
 
-
+console.log('Hola mundo');
 
 //Escuchar peticioznes
 app.listen( process.env.PORT, () => {
